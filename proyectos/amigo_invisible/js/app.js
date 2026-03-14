fb6b8d40b4164b32e7d5e385ae4429494df12ad3
dabemu <danielsecanet@gmail.com>
2026-03-14 16:22:54 +0100

    js amigoinvisible

--- proyectos/amigo_invisible/js/app.js
+++ proyectos/amigo_invisible/js/app.js
@@ -5,17 +5,27 @@ let nombre=document.getElementById("nombre")
 let solucion=document.getElementById("solucion")
 let botonResuelve=document.getElementById("botonResuelve")
 
-let amigos=[]
+let amigos= new Set()
 let solucionClientes = new Map()
 
+// funcion anonima
+const unoMas= function (nombre){
+    amigos.add(nombre)
+}
 
+// funcion flecha
+const otroMas= (nombre) => {
+    amigos.add(nombre)
+}
+unoMas("Laura")
+otroMas("Lola")
 
 // //funciones
 // function render(){
 //     console.log("AMIGOS: "+amigos)
 //     //console.log("CLIENTES: "+clientes)
 //     panel.innerHTML=""
-//     amigos.forEach((amigo)=>{
+//     amigos.forEach((amigo)=>
 //         console.log(amigo)
 //         panel.innerHTML+=`<input type='button' value='Borrar' data-id='${amigo}'>`+amigo+"<BR>"
 //     })
@@ -23,20 +33,23 @@ let solucionClientes = new Map()
 
 //funciones
 function render(){
-    console.log("AMIGOS: "+amigos)
+    console.log("AMIGOS: "+ Array.from(amigos))
     const elementos = document.createElement('ul')
-    panel.innerHTML=""
-    amigos.forEach((amigo)=>{
+    panel.innerHTML="AMIGOS: <br>"
+    Array.from(amigos).forEach((amigo)=>{
         console.log(amigo)
         let amigoLi = document.createElement('li')
         let button = document.createElement('input')
         let textoLi = document.createElement('span')
+        
         button.type='button'
         button.dataset.id=amigo
         button.value='Borrar'
         textoLi.innerHTML=amigo
-        amigoLi.append(button)
+        
         amigoLi.append(textoLi)
+        amigoLi.append("          ")
+        amigoLi.append(button)
         elementos.append(amigoLi)
     })
     panel.append(elementos)
@@ -53,8 +66,9 @@ function renderSolucion(){
 }
 
 function borraAmigo(nombre){
-    let indice=amigos.indexOf(nombre)
-    amigos.splice(indice,1)
+    //let indice=amigos.indexOf(nombre)
+    //amigos.splice(indice,1)
+    amigos.delete(nombre)
     render()
 }
 
@@ -66,20 +80,45 @@ function aleatorio(min, max) {
     devuelve false si el último se asigna a si mismo
 */
 function reparto(){
-    let quien=amigos.slice()
+    //copio los amigos    
+    let quienRegala= Array.from(amigos).slice()
+    let quienRecibe= Array.from(amigos).slice()
     solucionClientes = new Map()
     let esCorrecto=true
 
-    //TO DO: COMPLETA EL ALGORITMO PARA QUE FUNCIONE
+    //recorro los amigos que regalan
+    for (let i=0; i<quienRegala.length; i++){
+        let quienesRegalan = quienRegala[i];
+
+        //fuerzo a que no se regale a sí mismo
+        let opciones = quienRecibe.filter(r => r !== quienRegala[i]);
+
+        if (opciones.length === 0) {
+         return false;
+        }
+
+        // Elijo aleatoriamente a quién le regalo
+        let indiceAleatorio = aleatorio(0, opciones.length - 1);
+        let quienRecibeElegido = opciones[indiceAleatorio];
+
+        //guardo en un map amigo : cliente
+        solucionClientes.set(quienesRegalan, quienRecibeElegido);
+        
+        //elimino al amigo elegido de la lista de posibles receptores
+        let indiceReal = quienRecibe.indexOf(quienRecibeElegido);
+        quienRecibe.splice(indiceReal, 1);
+    }
 
     return esCorrecto    
 }
+
+
 
 //eventos
 botonAnyadir.addEventListener("click",(e) => {
     //capturar el formulario
     e.preventDefault()
-    amigos.push(nombre.value)
+    amigos.add(nombre.value)
     render()
 })
 
@@ -89,6 +128,16 @@ panel.addEventListener("click", (e)=>{
     console.log("Quieres borrar: "+ nombre)
     borraAmigo(nombre)
 })
+
+//no entiendo para qué sirve.
+//panel.addEventListener("click",(el)=>{
+//    if (el.target.dataset.action=="borrar"){
+//        let id=el.target.dataset.action
+//        let indice=amigos.indexOf(id)
+//        amigos.splice(indice,1)
+//        render2()
+//    }
+//})
 
 botonResuelve.addEventListener("click", (e)=>{
     console.log("Voy a resolver ")
@@ -102,8 +151,6 @@ botonResuelve.addEventListener("click", (e)=>{
     // c --> c
     renderSolucion()
 })
-
-
 
 
 
